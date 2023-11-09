ff309ea5becde8b73dc582f3a5a8691d679c6bdf
nandobfer <nandobfer@yahoo.com.br>
2023-11-09 09:43:29 -0300

    update: navigating to / when reference id changes

--- src/Providers.tsx
+++ src/Providers.tsx
@@ -1,13 +1,22 @@
 import { Snackbar, SnackbarProvider } from "burgos-snackbar"
-import React from "react"
+import React, { useEffect } from "react"
 import { IoProvider } from "./contexts/ioContext"
 import { TotalValueContextProvider } from "./contexts/totalValueContext"
+import { useSettings } from "./hooks/useSettings"
+import { useNavigate } from "react-router-dom"
 
 interface ProvidersProps {
     children: React.ReactNode
 }
 
 export const Providers: React.FC<ProvidersProps> = ({ children }) => {
+    const settings = useSettings()
+    const navigate = useNavigate()
+
+    useEffect(() => {
+        if (settings.referenceId) navigate("/")
+    }, [settings.referenceId])
+
     return (
         <SnackbarProvider>
             <IoProvider>
