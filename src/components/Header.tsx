751251726be573dcc6539046e90f229b23a36759
nandobfer <nandobfer@yahoo.com.br>
2024-03-11 11:23:44 -0300

    update: showing version

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,11 +2,12 @@ import React from 'react'
 import { Box, useMediaQuery } from '@mui/material'
 import logo from '../assets/logo.svg'
 import colors from "../style/colors"
+import { version } from "../../package.json"
 
 interface HeaderProps {}
 
 export const Header: React.FC<HeaderProps> = ({}) => {
-    const isMobile = useMediaQuery('(orientation: portrait)')
+    const isMobile = useMediaQuery("(orientation: portrait)")
 
     return (
         <Box
@@ -17,9 +18,11 @@ export const Header: React.FC<HeaderProps> = ({}) => {
                 height: isMobile ? "15vw" : "5vw",
                 borderBottom: `1px solid ${colors.border}`,
                 bgcolor: "white",
+                position: "relative",
             }}
         >
             <img src={logo} alt="Boz" style={{ width: isMobile ? "15vw" : "5vw" }} />
+            <Box sx={{ position: "absolute", bottom: 1, right: 1, color: colors.unactive, fontSize: "0.8rem" }}>{version}</Box>
         </Box>
     )
 }
